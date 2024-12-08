fdbd2222119b5a44f86db1f32470fdabfcf33a27
Almaz Amanzholuly <almaz.amanzholuly@nu.edu.kz>
2024-12-08 17:10:32 +0500

    Added: tables branch and group

--- backend/app/api/routes/branches.py
+++ backend/app/api/routes/branches.py
@@ -11,7 +11,7 @@ from app.models import Message
 router = APIRouter()
 
 
-@router.get("/", response_model=ItemsPublic)
+@router.get("/")
 def read_items(
     session: SessionDep, current_user: CurrentUser, skip: int = 0, limit: int = 100
 ) -> Any:
@@ -19,25 +19,5 @@ def read_items(
     Retrieve items.
     """
 
-    if current_user.is_superuser:
-        count_statement = select(func.count()).select_from(Item)
-        count = session.exec(count_statement).one()
-        statement = select(Item).offset(skip).limit(limit)
-        items = session.exec(statement).all()
-    else:
-        count_statement = (
-            select(func.count())
-            .select_from(Item)
-            .where(Item.owner_id == current_user.id)
-        )
-        count = session.exec(count_statement).one()
-        statement = (
-            select(Item)
-            .where(Item.owner_id == current_user.id)
-            .offset(skip)
-            .limit(limit)
-        )
-        items = session.exec(statement).all()
-
-    return ItemsPublic(data=items, count=count)
+    return {}
 
